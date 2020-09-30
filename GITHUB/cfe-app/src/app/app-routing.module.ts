40eb0fe83823b814cd4c143cacce76c1373eaa60
knudsjef <jeffery.knudson@gmail.com>
2020-09-30 15:07:55 -0500

    Added homepage component

--- GITHUB/cfe-app/src/app/app-routing.module.ts
+++ GITHUB/cfe-app/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './component/homepage/homepage.component';
+//import { LoginGuard } from './login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [];
+export const RoutingComponents = [HomepageComponent]
