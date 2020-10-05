b9da3f76133ee6fa0ca88d120671c7d3f289d5d0
prusta4555 <47877657+prusta4555@users.noreply.github.com>
2020-10-05 14:42:17 -0500

    Homepage Routing Setup

--- GITHUB/cfe-app/src/app/app-routing.module.ts
+++ GITHUB/cfe-app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './component/homepage/homepage.component';
+import { MultiplayerComponent } from './component/multiplayer/multiplayer.component';
+import { SandboxComponent } from './component/sandbox/sandbox.component';
+import { SingleplayerComponent } from './component/singleplayer/singleplayer.component';
+
+
 //import { LoginGuard } from './login.guard';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent}
+  { path: '', component: HomepageComponent },
+  { path: 'singleplayer', component: SingleplayerComponent },
+  { path: 'multiplayer', component: MultiplayerComponent },
+  { path: 'sandbox', component: SandboxComponent }
 ];
 
 @NgModule({
@@ -12,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponents = [HomepageComponent]
+export const RoutingComponents = [HomepageComponent, SingleplayerComponent, MultiplayerComponent, SandboxComponent]
