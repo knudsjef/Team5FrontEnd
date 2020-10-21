4a5d9cfadcaede28d60edc2dad727146f691389b
knudsjef <jeffery.knudson@gmail.com>
2020-10-21 15:28:50 -0500

    Layer cards correctly

--- GITHUB/cfe-app/src/app/component/card/card.component.ts
+++ GITHUB/cfe-app/src/app/component/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit} from '@angular/core';
+import { SandboxComponent } from '../sandbox/sandbox.component';
 
 @Component({
   selector: 'app-card',
@@ -9,9 +10,22 @@ export class CardComponent implements OnInit {
   @Input() type: string;
   @Input() number: string;
 
-  constructor() { }
+  public lastClickedCard: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    while(SandboxComponent.GetInstance() == null)
+    {
+      //Wait for SandboxComponent to be initialized
+    }
+    SandboxComponent.GetInstance().cardComponents.push(this);
+  }
+
+  public ChangeClass(): void
+  {
+    SandboxComponent.GetInstance().ClearCardsClasses(this);
   }
 
 }
--- GITHUB/cfe-app/src/app/component/sandbox/sandbox.component.ts
+++ GITHUB/cfe-app/src/app/component/sandbox/sandbox.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CardComponent } from '../card/card.component';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-sandbox',
@@ -9,7 +11,14 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 export class SandboxComponent implements OnInit {
 
-  constructor() { }
+  public cardComponents: CardComponent[] = [];
+  private static instance: SandboxComponent;
+  constructor() {
+    if (SandboxComponent.instance == null)
+    {
+      SandboxComponent.instance = this;
+    }
+   }
 
   cards: any[] =
   [
@@ -27,9 +36,24 @@ export class SandboxComponent implements OnInit {
       type: "Diamonds",
       value: "3",
       fileName: '3.jpg'
-    },
+    }
   ];
 
   ngOnInit(): void {
   }
+
+  public ClearCardsClasses(cardToSetTrue: CardComponent): void
+  {
+    for (var i = 0; i < this.cardComponents.length; i++)
+    {
+      this.cardComponents[i].lastClickedCard = false;
+    }
+    cardToSetTrue.lastClickedCard = true;
+  }
+
+  public static GetInstance(): SandboxComponent
+  {
+    return SandboxComponent.instance;
+  }
+
 }
