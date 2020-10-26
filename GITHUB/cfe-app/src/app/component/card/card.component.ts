563aca9e449c9508d1ed03328f165717eca2a3f2
knudsjef <jeffery.knudson@gmail.com>
2020-10-26 13:47:06 -0500

    Added GetPosition method
    Stores the x and y positions on the card objects when mouse up occurs

--- GITHUB/cfe-app/src/app/component/card/card.component.ts
+++ GITHUB/cfe-app/src/app/component/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { SandboxComponent } from '../sandbox/sandbox.component';
 
 @Component({
@@ -9,7 +9,8 @@ import { SandboxComponent } from '../sandbox/sandbox.component';
 export class CardComponent implements OnInit {
   @Input() type: string;
   @Input() number: string;
-
+  xPos: Number;
+  yPos: Number;
   public lastClickedCard: boolean = false;
 
   constructor() {
@@ -26,6 +27,13 @@ export class CardComponent implements OnInit {
   public ChangeClass(): void
   {
     SandboxComponent.GetInstance().ClearCardsClasses(this);
+  }
+
+  public GetPosition(e): void
+  {
+    this.xPos = e.clientX;
+    this.yPos = e.clientY;
+    console.log(this.xPos + ", " + this.yPos);
   }
 
 }
