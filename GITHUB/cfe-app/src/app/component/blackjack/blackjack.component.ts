e2fc2c2055131f6260fd7c170fae1ec60ab5ae9c
nimmoa5845 <nimmoa5845@my.uwstout.edu>
2020-10-28 15:54:19 -0500

    testing

--- GITHUB/cfe-app/src/app/component/blackjack/blackjack.component.ts
+++ GITHUB/cfe-app/src/app/component/blackjack/blackjack.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendApiService } from 'src/app/services/backend-api.service';
 
 @Component({
   selector: 'app-blackjack',
@@ -7,9 +8,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlackjackComponent implements OnInit {
 
-  constructor() { }
+  playerID:String;
+  gameID:number;
+
+  
+  constructor(private backendApiService: BackendApiService) { }
 
   ngOnInit(): void {
+    this.gameID=1;
+    this.playerID="player1";
+    this.hostGame();
+    this.setup();
+    this.deal();
+    
+  }
+
+  hostGame(){
+    var dict={};
+    dict["gameID"]=this.gameID;
+    dict["gameType"]="blackjack";
+    this.backendApiService.backendRequest("hostGame",dict).subscribe(obj =>{
+      console.log(obj);
+    });
+    this.setup();
+  }
+  setup(){
+    var dict={};
+    dict["gameID"]=this.gameID;
+    dict["method"]="setup";
+    dict["numPlayers"]=1;
+    this.backendApiService.backendRequest("blackjack",dict).subscribe(obj =>{
+      console.log(obj);
+    });
+  }
+  deal(){
+    var dict={};
+    dict["gameID"]=this.gameID;
+    dict["method"]="deal";
+    this.backendApiService.backendRequest("blackjack",dict).subscribe(obj =>{
+      console.log(obj);
+    });
+  }
+  hit(){
+    var dict={};
+    dict["gameID"]=this.gameID;
+    dict["method"]="hit";
+    dict["hand"]=this.playerID;
+    this.backendApiService.backendRequest("blackjack",dict).subscribe(obj =>{
+      console.log(obj);
+    });
+  }
+  stay(){
+    var dict={};
+    dict["gameID"]=this.gameID;
+    dict["method"]="stay";
+    dict["hand"]=this.playerID;
+    this.backendApiService.backendRequest("blackjack",dict).subscribe(obj =>{
+      console.log(obj);
+    });
+  }
+  updateHand(){
+    var dict={};
+    dict["gameID"]=this.gameID;
+    dict["method"]="getHand";
+    dict["hand"]=this.playerID;
+    this.backendApiService.backendRequest("blackjack",dict).subscribe(obj =>{
+      console.log(obj);
+    });
   }
 
 }
