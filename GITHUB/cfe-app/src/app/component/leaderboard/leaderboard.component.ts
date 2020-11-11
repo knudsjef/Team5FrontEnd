09ddd34d2c945e2db3fcaebe1e6975563bec5cfa
gomez902 <55208805+gomez902@users.noreply.github.com>
2020-11-11 16:05:26 -0600

    Little changes to leaderboard and comments

--- GITHUB/cfe-app/src/app/component/leaderboard/leaderboard.component.ts
+++ GITHUB/cfe-app/src/app/component/leaderboard/leaderboard.component.ts
@@ -14,30 +14,40 @@ export interface Scores {
 })
 
 export class LeaderboardComponent implements OnInit {
-
-  dataSource;
+  /** Data Source for table */
+  private dataSource;
+  /** Columns to be displayed in table */
   displayedColumns: string[] = ['Name', "Score"];
 
   constructor(private backendApiService: BackendApiService) {
-
 
   }
   
 
   
   ngOnInit(): void {
+    /**Make table load black jack leaderboard by default */
     var dict = {}
     dict["gameType"] = "BlackJack";
     this.backendApiService.backendRequest("getLeaderboard",dict).subscribe(obj =>{this.dataSource = obj.results}
       );
 
   }
+
+  /**@description Makes request from server for specified game leaderboard */
+  /**@param String Name of game that leaderboard is requested for */
   public getLeaderboard(gameName: String){
     var dict ={};
     dict["gameType"]=gameName;
     this.backendApiService.backendRequest("getLeaderboard",dict).subscribe(obj =>{this.dataSource = obj.results}
       );
+  }
 
+  /**@description Getter for table Data Source*/
+  /**@returns Returns the table data source */
+  public getDataSource(){
+
+    return this.dataSource;
   }
 }
 
