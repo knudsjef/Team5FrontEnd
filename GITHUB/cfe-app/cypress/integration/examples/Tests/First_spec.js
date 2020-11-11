beba014500290b971f8361b2eebb6d8ff3bf7a38
WoitteConnor <crwoitte@gmail.com>
2020-11-11 15:59:59 -0600

    add tests, comment code, remove unused file

--- GITHUB/cfe-app/cypress/integration/examples/Tests/First_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Health Test', function () {
-    it('Checks health of the homepage', function () {
-        cy.visit('http://localhost:4200/')
-        cy.get('.navbar-image')
-        .should('be.visible')
-    })
-})
