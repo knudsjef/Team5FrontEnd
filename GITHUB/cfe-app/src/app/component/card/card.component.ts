a0b3ef56b173274ef16ae924f447c1a46d90befe
knudsjef <jeffery.knudson@gmail.com>
2020-11-18 15:17:32 -0600

    Made card not rely on sandbox initilaiztion

--- GITHUB/cfe-app/src/app/component/card/card.component.ts
+++ GITHUB/cfe-app/src/app/component/card/card.component.ts
@@ -42,11 +42,6 @@ export class CardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    while(SandboxComponent.GetInstance() == null)
-    {
-      //Wait for SandboxComponent to be initialized
-    }
-    SandboxComponent.GetInstance().cardComponents.push(this);
     this.GetImageSource();
   }
 
