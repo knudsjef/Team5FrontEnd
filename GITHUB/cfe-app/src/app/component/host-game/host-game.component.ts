ac3833140cecab4e8c038ed7ce904fb15a03f60e
WoitteConnor <crwoitte@gmail.com>
2020-12-02 14:50:58 -0600

    change host/join game icons

--- GITHUB/cfe-app/src/app/component/host-game/host-game.component.ts
+++ GITHUB/cfe-app/src/app/component/host-game/host-game.component.ts
@@ -20,6 +20,7 @@ export class HostGameComponent implements OnInit {
     dict["gameType"]="blackjack";
     this.backendApiService.backendRequest("hostGame",dict).subscribe(obj =>{
       console.log(obj);
+      this.gameID = obj.gameID;
     });
     console.log(this.gameID);
   }
